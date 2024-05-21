6ba9f395c9a0168e32005a195f5c1fed4d1c2559
Xarist <artemsmolyaninov@gmail.com>
2024-05-21 11:43:27 +0300

    exercise 1

---
+++ data_download.py
@@ -0,0 +1,17 @@
+import yfinance as yf
+
+
+def fetch_stock_data(ticker, period='1mo'):
+    stock = yf.Ticker(ticker)
+    data = stock.history(period=period)
+    return data
+
+
+def add_moving_average(data, window_size=5):
+    data['Moving_Average'] = data['Close'].rolling(window=window_size).mean()
+    return data
+
+
+def calculate_and_display_average_price(data):
+    data['Average_Price'] = data['Close'].mean()
+    return data
